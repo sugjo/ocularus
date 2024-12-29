11dd4fb0d07414eb12c97773eda06e06ee4c68e9
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-12-29 22:06:26 +0300

    Add argument validations

--- src-dotnet/src/Program.cs
+++ src-dotnet/src/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿foreach (var arg in args)
+{
+	if (string.IsNullOrWhiteSpace(arg))
+	{
+		Console.WriteLine($"[WARNING]: The '{arg}' was null, empty or consist only of white spaces. Skipping...");
+		continue;
+	}
+
+	if (!File.Exists(arg))
+	{
+		Console.WriteLine($"[WARNING]: The file on '{arg}' path does not exists. Skipping...");
+		continue;
+	}
+}
