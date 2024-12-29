87fe0c4bf97526ef20c7617bf0fd2da7ffb20fee
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-12-29 22:27:50 +0300

    Extract icons using extractors

--- src-dotnet/src/Program.cs
+++ src-dotnet/src/Program.cs
@@ -1,4 +1,9 @@
-﻿foreach (var arg in args)
+﻿using Sugjo.Ocularus.Iconifier.Extractors;
+
+var peIconExtractor = new PEIconExtractor();
+var urlIconExtractor = new UrlIconExtractor();
+
+foreach (var arg in args)
 {
 	if (string.IsNullOrWhiteSpace(arg))
 	{
@@ -9,6 +14,20 @@
 	if (!File.Exists(arg))
 	{
 		Console.WriteLine($"[WARNING]: The file on '{arg}' path does not exists. Skipping...");
+		continue;
+	}
+
+	if (Path.GetExtension(arg) is ".ico" or ".exe" or ".dll")
+	{
+		await peIconExtractor.ExtractToAsync(arg);
+	}
+	else if (Path.GetExtension(arg) is ".url")
+	{
+		await urlIconExtractor.ExtractToAsync(arg);
+	}
+	else
+	{
+		Console.WriteLine($"[WARNING]: The file extension is not implemented. Skipping...");
 		continue;
 	}
 }
