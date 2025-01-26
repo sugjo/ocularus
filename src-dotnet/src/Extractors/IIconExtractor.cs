772177aba525e2e50af7ae570d258ce5fee8415d
sugjo <iluxa-com2001@yandex.ru>
2025-01-26 17:33:32 +0500

    add: integrate redplcs icon parser to ocularus build

--- src-dotnet/src/Extractors/IIconExtractor.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Sugjo.Ocularus.Iconifier.Extractors;
-
-public interface IIconExtractor
-{
-	Task ExtractToAsync(string filePath, string? outputPath = null, CancellationToken cancellationToken = default);
-}
